a510a64b1c5ccfd4390a24f76f0e982413dc2c6c
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-04-21 22:55:28 +0530

    Create Rotting Oranges.java

---
+++ Rotting Oranges.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[] {i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+        if (fresh == 0) {
+            return 0;
+        }
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int minutes = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                for (int[] dir : dirs) {
+                    int x = curr[0] + dir[0];
+                    int y = curr[1] + dir[1];
+                    if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        q.offer(new int[] {x, y});
+                        fresh--;
+                    }
+                }
+            }
+            minutes++;
+        }
+        return fresh == 0 ? minutes - 1 : -1;
+    }
+}
