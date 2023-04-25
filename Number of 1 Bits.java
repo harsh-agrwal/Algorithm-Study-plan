d35b76bcc3e3f1c33e800613eae18d0a6d96483f
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-04-25 20:48:34 +0530

    Create Number of 1 Bits.java

---
+++ Number of 1 Bits.java
@@ -0,0 +1,11 @@
+public class Solution {
+  public int hammingWeight(int n) {
+    int ans = 0;
+
+    for (int i = 0; i < 32; ++i)
+      if (((n >> i) & 1) == 1)
+        ++ans;
+
+    return ans;
+  }
+}
